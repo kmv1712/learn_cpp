99428a067800ce23bdd9c22481ea0f1c5a980968
KarpovM <kmv1712@gmail.com>
2020-10-07 09:48:03 +0500

    Дочитал до 96, выполнял упражнения, прочитал резюме по второму разделу

---
+++ douson_milkle/second_part/exercise/exercise_three.cpp
@@ -0,0 +1,21 @@
+// Игра где человек загадывает число, а компьютер пытается угадать
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{	
+	int hidden_number = 0, computer_number = 0, tries = 0;
+	cout << "Загадайте число от 0 до 100 " << endl;
+	cin >> hidden_number;
+
+	while (computer_number == hidden_number)
+	{ 	
+		++tries;
+		srand(static_cast<unsigned int>(time(0)));
+	    computer_number = rand() % 100 + 1;
+	}
+	cout << "Компьютер угадал с :" << tries;
+	return 0;
+
+}
