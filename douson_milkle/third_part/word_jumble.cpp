c731adeaf8a85032aab910ac36ac968c0256b191
KarpovM <kmv1712@gmail.com>
2020-10-09 09:47:11 +0500

    Прочитал до 121 стр. прочитал про двухмерный и одномерный массив, про си-строки, повторил сСЛовомеску

---
+++ douson_milkle/third_part/word_jumble.cpp
@@ -0,0 +1,61 @@
+// Игра Словомеска
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+	enum fields {WORD, HINT, NUM_FIELDS};
+	const int NUM_WORDS = 5;
+	const string WORDS[NUM_WORDS][NUM_FIELDS] = 
+	{
+		{"wall", "Do you feel you're banging your head against something?"},
+		{"glasses", "These might help you see the answer."},
+		{"labored", "Going slowly. is it?"},
+		{"persistent", "Keep at it."},
+		{"jumble", "It's what the game is all about."}
+	};
+	srand(static_cast<unsigned int>(time(0)));
+	int choice = (rand() % NUM_WORDS);
+	string theWord = WORDS[choice][WORD]; // Слово которое нужно угадать
+	string theHint = WORDS[choice][HINT]; // Подсказка к слову
+	string jumble = theWord;
+	int length = jumble.size();
+	for (int i = 0; i < length; ++i)
+	{
+		int index1 = rand() % length;
+		int index2 = rand() % length;
+		char temp = jumble[index1];
+		jumble[index1] = jumble[index2];
+		jumble[index2] = temp;
+	}
+	cout <<  "\t\t\tWelcome to Word Jumble!\n\n";
+	cout << "Unscramblе the letters to make а word. \n";
+	cout << "Enter 'hint' for а hint. \n";
+	cout << "Enter 'exit' to exit the game. \n\n";
+	cout  << "The jumble is: " <<  jumble;
+	string guess;
+	cout << "\n\nYour guess: ";
+	cin >> guess;
+	while ((guess != theWord) && (guess != "exit"))
+	{
+		if (guess == "hint")
+		{
+			cout << theHint;
+		}
+		else
+		{
+			cout << "Sorry, that's not it.";
+		}
+		cout << "\n\nYour guess: ";
+		cin >> guess;
+	}
+	if (guess == theWord)
+	{
+		cout << "\nThat's it! You guessed it!\n";
+	}
+	cout << "\nThanks for playing.\n";
+	return 0;
+}
+
