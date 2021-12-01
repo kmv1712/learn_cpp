f5f8c8749833150b9560d0184af5642b95a2dd90
Mihail <kmv1712@gmail.com>
2021-12-01 09:36:46 +0500

    Решил задачку 6.4

--- tasks/64_format_numbers_to_column.c
+++ tasks/64_format_numbers_to_column.c
@@ -4,6 +4,28 @@
 
 int main()
 {
+    int n;
+    float arr[100], num, max = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%f", &num);
+        arr[i] = num;
+        if (num > max)
+            max = num;
+    }
+
+    int pow = 10, width = 1;
+    while(max >= pow)
+    {
+        pow *= 10;
+        width += 1;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        printf("%*.2f\n", width+3, arr[i]);
+    }
 
     return 0;
 }
