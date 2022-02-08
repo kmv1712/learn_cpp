360ddb2093730391fba87654ff8aa1fecdc09f2c
Mihail <kmv1712@gmail.com>
2022-02-08 09:25:19 +0500

    Решил задачку 101

---
+++ tasks/101_output_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print(int n)
+{
+    if( n > 1 )
+        print( n-1 );
+    printf( "%d\n", n );
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    print(n);
+    return 0;
+}
